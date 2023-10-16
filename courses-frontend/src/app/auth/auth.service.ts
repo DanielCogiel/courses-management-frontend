632838eadc778f29991e80df2e0bc6e2fa7dd1da
DanielCogiel <dcogiel.work@gmail.com>
2023-10-16 19:29:32 +0200

    Dodanie strony logowania

--- courses-frontend/src/app/auth/auth.service.ts
+++ courses-frontend/src/app/auth/auth.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import Role from "./models/role.enum";
 import { ApiService } from "../api/api.service";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  role$: BehaviorSubject<Role | null> = new BehaviorSubject<Role | null>(null);
   constructor(private _apiService: ApiService) {}
   login(data: {username: string, password: string}) {
-    return this._apiService.post<{token: string | null, role?: Role}>('/login', data);
+    return this._apiService.post<{message: string, token: string | null, role?: Role}>('/login', data);
   }
   register(data: any) {
     return this._apiService.post<{message?: string}>('/register', data);
   }
-  setToken(token: string) {
-    localStorage.setItem('token', token);
+  setRole(role?: Role | null) {
+    this.role$.next(role || null);
+  }
+  setToken(token?: string | null) {
+    if (token)
+      localStorage.setItem('token', token);
   }
   destroyToken() {
     localStorage.removeItem('token');
