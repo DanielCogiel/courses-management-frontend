96d74c605e105baa4bb1952e8324e104f4090832
Daniel Cogiel <72274710+DanielCogiel@users.noreply.github.com>
2024-07-23 11:25:39 +0200

    Dodanie możliwości definiowania zmiennych środowiskowych (#14)

--- courses-frontend/src/app/api/api.config.ts
+++ courses-frontend/src/app/api/api.config.ts
@@ -1,3 +1,3 @@
 //TODO get from .env
-export const SERVER_URL = 'http://localhost:3000';
+export const SERVER_URL = process.env['SERVER_URL'] || 'http://localhost:3000';
 export const API_URL = `${SERVER_URL}/api`;
